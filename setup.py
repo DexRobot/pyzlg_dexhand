96257734d37e5fce25dee6989ba22a3897a61f6c
Xiang Lei <xiang.lei.se@foxmail.com>
2024-12-12 10:53:42 +0800

    chore: Update dependencies and formatting in setup.py
    - Refactor `setup.py` to improve dependency management and formatting
    - Add new dependencies to the `deps` extra_require in `setup.py`

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     name="pyzlg_dexhand",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "pyyaml"
-    ],
+    install_requires=["numpy", "pyyaml"],
     extras_require={
-        'test': [
-            'pytest',
-            'pytest-cov'
+        "test": ["pytest", "pytest-cov"],
+        "docs": [
+            "sphinx>=8.1.3",
+            "sphinx-rtd-theme>=1.3.0",
+            "sphinx-autodoc-typehints",
         ],
-    }
+    },
 )
